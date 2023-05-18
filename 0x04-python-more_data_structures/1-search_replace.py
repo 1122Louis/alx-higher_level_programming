1bf70f07a4b8bf24210b60a83721267b1a9a4edf
1122Louis <louisdzidzor22@gmail.com>
2023-05-18 03:07:34 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    """Search and replace in list"""
     size = len(my_list)
     new_list = my_list.copy()
     for i in range(size):
