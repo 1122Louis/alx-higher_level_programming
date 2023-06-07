33f12381785be4a994793ff21b0a13bcb0ce1ebd
1122Louis <louisdzidzor22@gmail.com>
2023-06-06 17:27:18 -0700

    deleted

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for row in range(len(matrix)):
-        for item in range(len(matrix[row])):
-            if item != 0:
-                print(" ", end="")
-            print("{:d}".format(matrix[row][item]), end="")
-        print()
--- 0x04-python-more_data_structures/1-search_replace.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def search_replace(my_list, search, replace):
-    size = len(my_list)
-    new_list = my_list.copy()
-    for i in range(size):
-        if new_list[i] == search:
-            new_list[i] = replace
-    return new_list
