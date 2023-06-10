7e57d2e1d5dc8e397285c106b4f60b52652f1e88
1122Louis <louisdzidzor22@gmail.com>
2023-06-10 08:39:00 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    import hidden_4
-    names = dir(hidden_4)
-    for i in names:
-        if i[:2] != "__":
-            print(i)
+    import sys
+    num = len(sys.argv)
+    sum = 0
+    if num > 1:
+        for i in range(1, num):
+            sum += int(sys.argv[i])
+        print("{}".format(sum))
+    else:
+        print("{}".format(sum))
