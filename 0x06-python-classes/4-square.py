d8c3ec99efdd3a6f08e09905748c39cde7e82651
1122Louis <louisdzidzor22@gmail.com>
2023-06-28 09:54:18 -0700

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,49 +1,28 @@
 #!/usr/bin/python3
+# 4-square.py
+"""Access and update private attribute: """
 
-class Square:
-    """Class representing a square
-    """
+
+class Square(object):
+    """class variable size"""
     def __init__(self, size=0):
-        """Init Square
-
-        Attributes:
-            size: length of side of square
-        """
-        self.size = size
-
-    def area(self):
-        """Find area of square
-
-        Args:
-            size (int): length of side of square
-
-        Returns:
-            int: area of square
-        """
-        return self.__size * self.__size
+        """initialize size"""
+        self.__size = size
 
     @property
     def size(self):
-        """Getter
-
-        Returns:
-            size (int): length of side of square
-        """
+        """Property"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Setter
-
-        Args:
-            value (int): natural number (>= 0)
-
-        Raises:
-            TypeError: if size is not an integer
-            ValueError: if size is less than 0
-        """
-        if (not isinstance(value, int)):
+        """class variable size"""
+        if type(value) != int:
             raise TypeError("size must be an integer")
-        if (value < 0):
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def area(self):
+        """Define area"""
+        return self.__size * self.__size
