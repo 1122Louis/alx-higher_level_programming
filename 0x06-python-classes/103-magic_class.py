e4c68a376fb14c77ef531f39e706aed40a036688
1122Louis <louisdzidzor22@gmail.com>
2023-06-28 10:16:23 -0700

    103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,47 +1,26 @@
 #!/usr/bin/python3
-import math
+"""Define a MagicClass for the bytecode provided by ALX."""
 
-"""
-Module 103-magic_class.py
-Defines a circle class
-Takes in a radius on initialization
-Defines methods for finding area and perimeter of the circle
-"""
+import math
 
 
 class MagicClass:
-    """Class representing a magic structure, likely a circle"""
-    def __init__(self, radius):
-        """Init MagicClass
+    """Represent a circle."""
 
-        Args:
-            radius (number): int or float for radius
-
-        Raises:
-            TypeError: if radius is not a number
+    def __init__(self, radius=0):
+        """Initialize a MagicClass.
+        Arg:
+            radius (float or int): The radius of the MagicClass.
         """
-        if (not (isinstance(radius, int) or isinstance(radius, float))):
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        """Find area of a circle
-
-        Args:
-            radius (number): radius of the circle
-
-        Returns:
-            number: area of a circle
-        """
-        return self.__radius**2 * math.pi
+        """Return the area of the MagicClass."""
+        return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-        """Find perimeter of a circle
-
-        Args:
-            radius (number): radius of the circle
-
-        Returns:
-            number: perimeter of a circle
-        """
-        return 2 * math.pi * self.__radius
+        """Return The circumference of the MagicClass."""
+        return (2 * math.pi * self.__radius)
