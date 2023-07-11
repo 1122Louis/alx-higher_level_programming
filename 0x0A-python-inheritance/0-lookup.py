bd17cf988e812161aed8806df75ea4f450cfe7e6
1122Louis <louisdzidzor22@gmail.com>
2023-07-11 01:49:07 +0000

    initial commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+
+dule 0-lookup
+Find the available attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """Return the available attributes and methods of an object
+
+    Args:
+        obj: object to check
+
+    Return:
+        a list object
+    """
+    return dir(obj)
