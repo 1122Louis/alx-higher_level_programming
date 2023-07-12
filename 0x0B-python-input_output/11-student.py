7ecee1665b51160e05b0e589fe2c908e58415410
Louis Dzidzor <louisdzidzor22@gmail.com>
2023-07-12 18:14:21 +0000

    0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+Module 10-student
+
+Contains a class defining a Student
+"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Represent a student in JSON format
+
+        Args:
+            attrs: filter keys
+
+        Return:
+            dictionary representation of object
+        """
+        if (attrs is None):
+            return self.__dict__
+        return dict(filter(lambda k: k[0] in attrs, self.__dict__.items()))
+
+    def reload_from_json(self, json):
+        """Load data from json into object
+
+        Args:
+            json: text to load into object
+        """
+        for k, v in json.items():
+            self.__dict__[k] = v
