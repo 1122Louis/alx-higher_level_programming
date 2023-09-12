b6eecf9d2426a15775b5d1d21d373db6160ee258
Louis Dzidzor <louisdzidzor22@gmail.com>
2023-09-11 17:11:59 -0700

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const phrases = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let i = 0;
-while (i < phrases.length) {
-  console.log(phrases[i]);
-  i++;
-}
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+languages.forEach(x => console.log(x));
