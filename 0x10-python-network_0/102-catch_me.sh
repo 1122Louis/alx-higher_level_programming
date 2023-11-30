a9f07836abddcd86cf792457139404784a44ddee
Louis Dzidzor <louisdzidzor22@gmail.com>
2023-11-30 14:05:02 -0800

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Find the response 'You got me' from the URL below
-curl -s -X PUT -H "user_id: 98" -b "user_id = 98" -L 0.0.0.0:5000/catch_me
+# Script that makes a request to causes an specific response
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:HolbertonSchool"
