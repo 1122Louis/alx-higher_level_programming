89bcd5b7ce60a092bf6832f4e3c92768cceaa81b
Louis Dzidzor <louisdzidzor22@gmail.com>
2023-12-01 09:58:33 -0800

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,33 +1,14 @@
 #!/usr/bin/python3
+"""Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id
 """
-Python script that sends a search request to the
-Star Wars API, searching people
-"""
-import requests
 import sys
+import requests
 
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
 
-if __name__ == "__main__":
-    page = 1
-
-    films = {}
-
-    for num in range(1, 8):
-        url = 'https://swapi.co/api/films/{}/'.format(num)
-        r = requests.get(url)
-        films[url] = r.json().get('title')
-
-    while page:
-        s = sys.argv[1]
-        url = 'https://swapi.co/api/people/?search={}&page={}'.format(s, page)
-        r = requests.get(url)
-        json_o = r.json()
-        if page == 1:
-            print("Number of results: {}".format(json_o.get('count')))
-        for character in json_o.get('results'):
-            print(character.get('name'))
-            for film in character.get('films'):
-                print("\t{}".format(films[film]))
-        if not json_o.get('next'):
-            break
-        page += 1
+    res = requests.post(
+        url, auth=requests.auth.HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+    res_json = res.json()
+    print(res_json.get('id'))
